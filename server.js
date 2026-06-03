4acedeb5ac17c340d101b64a3dd266a8380848b1
freddy879 <147188478+freddy879@users.noreply.github.com>
2026-06-02 20:52:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ console.log("DB:",   process.env.MONGO_DB || 'agro-123');
 // ================== MONGO ==================
 // Si Render no lee las variables de entorno, usará los valores de la derecha por defecto
 const user = process.env.MONGO_USER || 'agro_2026';
-const pass = encodeURIComponent(process.env.MONGO_PASS || 'FnGw36LekUz0Fla5');
+const pass = encodeURIComponent(process.env.MONGO_PASS || 'agro');
 const db   = process.env.MONGO_DB || 'agro-123'; 
 
 const URI = `mongodb+srv://${user}:${pass}@cluster0.8otlbi7.mongodb.net/${db}?retryWrites=true&w=majority`;
