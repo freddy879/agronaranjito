05399e81a07f7924b864200bb6caa7613723dda6
freddy879 <147188478+freddy879@users.noreply.github.com>
2026-06-04 00:42:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Resend } = require('resend');
 
 const app = express();
 
-app.use(cors());      
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('.'));
